5fddd644ebb007ae8693562f75a1a70547d37bea
mahaveer <mahaveersharma0496@yahoo.com>
2017-06-24 16:12:14 +0530

    navbar change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => (
   <nav className="navbar navbar-toggleable-sm navbar-inverse bg-inverse">
-    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <Link className="navbar-brand" to="/">Navbar</Link>
 
-    <div className="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault" >
+    <div className="collapse navbar-collapse justify-content-end" id="navbar" >
       <ul className="navbar-nav ">
         <li className="nav-item">
           <Link className="nav-link" to="/register">Sign-Up</Link>
